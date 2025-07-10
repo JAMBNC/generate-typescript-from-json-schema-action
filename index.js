69e198885fca453608b714d9d8409faa355e45f4
Austin M <austin.meier@hotmail.com>
2025-07-10 12:50:41 -0400

    make rexport index

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ import { resolveRefs } from "json-refs";
 import { format } from "prettier";
 
 const generate = async (dir) => {
+  let indexExport = '';
   const files = fs.readdirSync(dir)
 
   for (const file of files) {
     const name = file.split('.')[0];
+    const fileName = `${name}.ts`
     const jsonSchema = JSON.parse(fs.readFileSync(`${dir}/${file}`, { encoding: 'utf8' }))
     const { resolved } = await resolveRefs(jsonSchema);
     const zodSchema = jsonSchemaToZod(resolved, {
@@ -19,8 +21,10 @@ const generate = async (dir) => {
       type: true,
     })
     const formatted = await format(zodSchema, { parser: "typescript" });
-    fs.writeFileSync(`${name}.ts`, formatted)
+    fs.writeFileSync(fileName, formatted)
+    indexExport += `export * from './${fileName}';\n`
   }
+  fs.writeFileSync('index.js', indexExport)
 }
 
 try {
@@ -38,7 +42,8 @@ try {
   process.chdir(typesDir);
 
   /* Generate the schemas */
-  await generate(`../${schemaDir}`)
+  await generate(`../${schemaDir}`, index)
+
 
   await exec.exec('git', ['add', '.']);
   await exec.exec('git', ['commit', '--allow-empty', '-m', 'Schema type update']);
