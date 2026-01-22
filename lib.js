57d63f82f98f26c93a9e57edc2784a74462268cc
Austin M <austin.meier@hotmail.com>
2026-01-22 12:10:15 -0700

    update to allow manual

--- lib.js
+++ lib.js
@@ -1,6 +1,6 @@
 import fs from "node:fs";
 import path from "node:path";
-import exec from "@actions/exec"
+import exec from "@actions/exec";
 import { jsonSchemaToZod } from "./json-schema-zod/index.js";
 import { resolveRefs } from "json-refs";
 import { format } from "prettier";
@@ -19,37 +19,43 @@ export const getAllFiles = (dirPath, files = []) => {
   }
 
   return files;
-}
+};
 
 export const generate = async (dir, outputDir) => {
-  let indexExport = '';
+  let indexExport = "";
 
-  const allFile = fs.readFileSync(`${dir}/All.json`, { encoding: 'utf8' })
-  const all = JSON.parse(allFile)
+  const allFile = fs.readFileSync(`${dir}/All.json`, { encoding: "utf8" });
+  const all = JSON.parse(allFile);
 
-  const res = await resolveRefs(all)
+  const manualSchemas = getAllFiles(`${dir}/manual`);
+  manualSchemas.forEach((file) => {
+    const schemaFile = fs.readFileSync(file, { encoding: "utf8" });
+    const schema = JSON.parse(schemaFile);
+    Object.assign(all["$defs"], schema["$defs"]);
+  });
+
+  const res = await resolveRefs(all);
 
   const defs = Object.values(res.refs).reduce((defs, ref) => {
-    defs[ref.uri] = ref.value
+    defs[ref.uri] = ref.value;
     return defs;
-  }, {})
+  }, {});
 
-  fs.mkdirSync(outputDir, { recursive: true })
+  fs.mkdirSync(outputDir, { recursive: true });
   for (const [type, schema] of Object.entries(defs)) {
-
-    const parts = type.split('/');
-    const name = (parts[parts.length - 1] ?? '').replace(/[^a-zA-Z0-9_$]/g, '');
+    const parts = type.split("/");
+    const name = (parts[parts.length - 1] ?? "").replace(/[^a-zA-Z0-9_$]/g, "");
 
     const zodSchema = jsonSchemaToZod(schema, {
       name: name,
       module: "esm",
       type: true,
-    })
+    });
 
     const formatted = await format(zodSchema, { parser: "typescript" });
 
-    fs.writeFileSync(`${outputDir}/${name}.ts`, formatted)
-    indexExport += `export * from './${name}';\n`
+    fs.writeFileSync(`${outputDir}/${name}.ts`, formatted);
+    indexExport += `export * from './${name}';\n`;
   }
-  fs.writeFileSync(`${outputDir}/index.ts`, indexExport)
-}
+  fs.writeFileSync(`${outputDir}/index.ts`, indexExport);
+};
